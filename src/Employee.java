be9d304ddda92349e74e4b63e2ec80bc8c20fbf2
Burak Tatar <mburaktatar@gmail.com>
2025-04-13 21:38:48 +0300

    Inıtial commit.

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Employee {
+    private String ad;
+    private String soyad;
+    private ContactInfo contactInfo;
+
+    // Constructor oluşturma;
+    public Employee(String ad, String soyad, String telefon, String email) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.contactInfo = new ContactInfo(telefon, email);
+    }
+
+    // Inner class (İletişim bilgileri için) oluşturma;
+    public class ContactInfo {
+        private String telefon;
+        private String email;
+
+        public ContactInfo(String telefon, String email) {
+            this.telefon = telefon;
+            this.email = email;
+        }
+
+        public void iletisimBilgileriniYazdir() {
+            System.out.println("Telefon: " + telefon);
+            System.out.println("E-posta: " + email);
+        }
+    }
+
+    // Tüm çalışan bilgilerini yazdıran metodu oluşturma;
+    public void calisanBilgileriniYazdir() {
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        contactInfo.iletisimBilgileriniYazdir();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    // Bir çalışan nesnesi oluşturulup bilgilerini ekrana yazdırma;
+    public static void main(String[] args) {
+        Employee calisan1 = new Employee("Burak", "Tatar", "0555 123 4567", "burak@example.com");
+
+        System.out.println("Çalışan Bilgileri:");
+        calisan1.calisanBilgileriniYazdir();
+    }
 }
